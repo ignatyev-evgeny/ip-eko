48f5e904d7c13aa3d97396ba03cd2d2948d90919
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-07-21 22:41:12 +0300

    Обновление роутингов

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::any('/', function () {
-    return view('welcome');
-});
+Route::withoutMiddleware(VerifyCsrfToken::class)->group(function () {
+    Route::any('/', function () {
+        return view('welcome');
+    });
 
-Route::any('/setup', function () {
-    return view('welcome');
-});
+    Route::any('/setup', function () {
+        return view('welcome');
+    });
 
-Route::any('/settings', function () {
-    return view('welcome');
+    Route::any('/settings', function () {
+        return view('welcome');
+    });
 });
