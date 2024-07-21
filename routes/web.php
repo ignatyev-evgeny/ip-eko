f6173b314facb3b487c25a3c7820bf25ba1f2fc2
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-07-21 22:26:24 +0300

    Добавлены роуты для создания приложения

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/setup', function () {
+    return view('welcome');
+});
+
+Route::get('/settings', function () {
+    return view('welcome');
+});
