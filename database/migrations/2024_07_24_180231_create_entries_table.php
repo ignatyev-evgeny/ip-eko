bd4fa75192dd543d060ff8fb1b15042b70f46a2d
Evgeny Ignatyev <ignatyev.ew@gmail.com>
2024-07-26 08:10:28 +0300

    Обновление миграции поступлений

--- database/migrations/2024_07_24_180231_create_entries_table.php
+++ database/migrations/2024_07_24_180231_create_entries_table.php
@@ -9,15 +9,15 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('entries', function (Blueprint $table) {
             $table->id();
-            $table->string('status', 100);
+            $table->string('status');
             $table->dateTime('datetime');
-            $table->string('number', 100);
+            $table->string('number');
             $table->decimal('amount', 10, 2);
-            $table->string('counteragent', 100);
-            $table->string('counteragent_bank_account', 100);
-            $table->string('contract', 100);
+            $table->string('counteragent');
+            $table->string('counteragent_bank_account');
+            $table->string('contract');
             $table->text('payment_purpose');
-            $table->string('operation_type', 100);
+            $table->string('operation_type');
             $table->timestamps();
 
             $table->index('datetime');
